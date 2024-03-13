caaf4688f7434b6829ecb5f54d21cf3bbe597bbb
YoungjaeRo <146312456+YoungjaeRo@users.noreply.github.com>
2024-03-13 13:38:56 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+   public static void main(String[] args) {
+       Scanner in  = new Scanner(System.in);
+       String lastString = "??!";
+       String s = in.next(); // 문자의 입력을 받는 스캐너
+       System.out.println(s + lastString);
+   } 
+    
+    
+}

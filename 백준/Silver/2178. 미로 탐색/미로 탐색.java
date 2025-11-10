b666c71b31c419a9895c5e962300e4fedf1d38f3
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-10 22:54:17 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,90 +1,89 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
 	static int N;
 	static int M;
 
 	static int[][] map;
+	static int[][] dist;
 
-	// 동서남북
-	static int[] dx = {0, 0, 1, -1};
-	static int[] dy = {1, -1, 0, 0};
+	static boolean[][] visited;
 
+	// 상, 하, 좌, 우
+	static final int[] dr = {-1, 1, 0, 0};
+	static final int[] dc = {0, 0, -1, 1};
 
 	static class Point {
 		int r;
 		int c;
-		 Point (int r, int c) {
-			 this.r = r;
-			 this.c = c;
-		 }
-	}
 
+		Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+
+		}
+	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-
-		// 그래프 초기화 ㅇㅇ
 		map = new int[N][M];
+		dist = new int[N][M];
+		visited = new boolean[N][M];
 
+
+		//  공백 없이 붙은 문자열을 입력 받음 ㅇㅇ
 		for(int i = 0; i < N; i++) {
 			String numbers = br.readLine();
 
 			for(int j = 0; j < M; j++) {
-				map[i][j] = numbers.charAt(j) - '0'; // 문자열로 들어온 숫자를 int 형으로 반환하게 해줌
+				map[i][j] = numbers.charAt(j) - '0';
 			}
 		}
 
-		int answer = bfs(); // 0,0에서 마지막 칸 까지 최단 칸수
-		System.out.println(answer);
+		System.out.println(bfs());
 
 	}
 
 	static int bfs() {
-		int[][] dist = new int[N][M]; // 0이면 미방문, 그 외는 거리수 더하기
-		Deque <Point> q = new ArrayDeque<>();
+		ArrayDeque<Point> q = new ArrayDeque<>();
+		visited[0][0] = true;
+		dist[0][0] = 1; // 시작 칸 포함함
 
-		//시작
-		dist[0][0] = 1;
 		q.offer(new Point(0, 0));
 
 		while(!q.isEmpty()) {
+
 			Point cur = q.poll();
-
-
-			// 도착지에 왔다면, 거리를 변환
+			
 			if(cur.r == N - 1 && cur.c == M - 1) {
 				return dist[cur.r][cur.c];
 			}
 
-			// 4방향 탐색 시작
 			for(int i = 0; i < 4; i++) {
-				int nr = cur.r + dx[i];
-				int nc = cur.c + dy[i];
+				int nr = cur.r + dr[i];
+				int nc = cur.c + dc[i];
 
-
-				// 밤위, 벽, 방문 여부 검사 ㄱㄱ
 				if(nr < 0 || nr >= N || nc < 0 || nc >= M) {
 					continue;
 				}
 
-				//벽과 이미 방문한 좌표는 방문 안함
-				if(map[nr][nc] == 0 || dist[nr][nc] != 0) {
+				if(map[nr][nc] == 0 || visited[nr][nc]) {
 					continue;
 				}
 
-				dist[nr][nc] = dist[cur.r][cur.c] + 1;
-				q.offer(new Point(nr, nc));
+				visited[nr][nc] = true;
+				dist[nr][nc] = dist[cur.r][cur.c] + 1; // 한칸을 이동했기 때문에
 
+				q.offer(new Point(nr, nc));
 			}
+
 		}
-		return 0; // 도착 불가시
+
+		return 0;
 	}
 }

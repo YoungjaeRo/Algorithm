ab245c4ca3f7cf803a87217f3b5f9e27023a2374
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-11 12:11:24 +0900

    [level 2] Title: 피로도, Time: 3.72 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,33 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+
 class Solution {
-   public int solution(int k, int[][] dungeons) {
-		boolean[] used = new boolean[dungeons.length];
-
-		return dfs(k, dungeons, used, 0);
-
-	}
-
-	// k: 현재 남은 피로도
-	// dungeons[i][0]: i번 던전 입장 최소 필요 피로도
-	// dungeons[i][1]: i번 던전 소모 피로도
-	// used[i]: i번 던전을 이미 돌았는지
-	// cnt: 지금까지 돈(클리어한) 던전 수
-
-
-	private int dfs(int k, int[][] dungeons, boolean[] used, int cnt) {
-		int best = cnt;
-
-
-		for(int i = 0; i < dungeons.length; i++) {
-			// 아직 안 갔고 입장 조건을 만족하면 시도
-			if(!used[i] && k >= dungeons[i][0]) {
-				used[i] = true;
-				best = Math.max(best, dfs(k - dungeons[i][1], dungeons, used, cnt + 1));
-
-				used[i] = false; // 백트래킹
-			}
-
-		}
-		
-		return best;
-	}
-}
+    static boolean[] visited;
+    
+    static int answer = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+       int n = dungeons.length;
+        
+        visited = new boolean[n];
+        
+        dfs(k, dungeons, 0);
+        
+        return answer;
+    }
+    
+    
+    // k: 현재 피로도, cnt: 지금까지 클리어한 던전 수
+    public void dfs(int k, int[][] dungeons, int cnt) {
+        // 더 못 가더라도 지금까지의 개수로 최댓값 갱신
+        answer = Math.max(answer, cnt);
+        
+        for(int i = 0; i < dungeons.length; i++) {
+            
+            int req = dungeons[i][0];
+            int cost = dungeons[i][1];
+            
+            if(visited[i]) { // 이미 입장한 던전은 스킵함
+                continue;
+            }
+            
+            // 입장 가능하면, 다른 던전 도전!
+            if(k >= req) {
+                visited[i] = true;
+                
+                dfs(k - cost, dungeons, cnt + 1);
+                
+                visited[i] = false; // 백트래킹
+                
+                }
+            }
+        }
+    }

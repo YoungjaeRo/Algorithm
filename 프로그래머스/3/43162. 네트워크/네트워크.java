ef91a730dd525aeaa14cdcfc8ec731df489c0f3a
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-11 18:19:09 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -9,27 +9,19 @@ class Solution {
         
         for(int i = 0; i < n; i++) {
             if(!visited[i]) {
-                bfs(i, computers, visited);
+                dfs(i, computers, visited);
                 networks++;
             }
         }
         return networks;
     }
     
-    public void bfs(int start, int[][]computers, boolean[] visited) {
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.add(start);
+    public void dfs(int start, int[][]computers, boolean[] visited) {
         visited[start] = true;
-        
-        while(!q.isEmpty()) {
-            
-            int cur = q.poll();
-            
-            for(int next = 0; next < computers.length; next++) {
-                if(computers[cur][next] == 1 && !visited[next]) {
-                    q.add(next);
-                    visited[next] = true;
-                }
+    
+        for(int next = 0; next < computers.length; next++) {
+            if(computers[start][next] == 1 && !visited[next]) {
+                dfs(next, computers, visited);
             }
         }
     }

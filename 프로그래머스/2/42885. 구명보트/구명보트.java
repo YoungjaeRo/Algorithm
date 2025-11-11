e3e3009a2dbe2ee0a5ca77f333c9f9bfa1f2618a
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-11 12:25:03 +0900

    [level 2] Title: 구명보트, Time: 13.24 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,29 +1,30 @@
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
+
 
 class Solution {
     public int solution(int[] people, int limit) {
-       Arrays.sort(people); // 오름차순으로 정렬
-
-		int i = 0; // 가장 가벼운 사람 인덱스
-		int j = people.length - 1; // 가장 무거운 사람 인덱스
-
-		int boats = 0;
-
-		while(i <= j) {
-			boats++; // 보트 태운 횟수 1증가
-
-
-			// 가장 무거운 j와 가장 가벼운 i를 왠만해선 같이 태워서 보내야함
-			if(people[i] + people[j] <= limit) {
-				i++; // 가벼운 사람도 탑승
-			}
-
-			j--; // 무거운 사람 탑승 처리
-
-
-
-		}
-		return boats;
-	}
-    
+        int boat = 0;
+        
+        Arrays.sort(people); // 일단은 오름차순으로 정렬함
+        
+        int i = 0;  //  가장 가벼운 사람 인덱스
+        
+        int j = people.length - 1; // 가장 무거운 사람의 인덱스
+        
+        while(i <= j) {
+            
+            // 일단 보트 하나는 무조건 나감
+            boat++;
+            
+            if(people[i] + people[j] <= limit) {
+                i++; // 가벼운 사람도 같이 탈 수 있으면, 가벼운 사람도 태움
+            }
+            
+            j--;
+        }
+        
+        return boat;
+        
+    }
 }

082e466ed95280674a24c9912e3676f2f0b5b251
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-17 16:31:42 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 27044 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,51 +1,38 @@
+import java.io.*;
+import java.util.*;
 
-import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-        // 수열의 개수 받기
-        int N = scanner.nextInt();
-        int A[] = new int[N];
+		StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < N; i++){
-            A[i] = scanner.nextInt();
-        }
+		Stack<Integer> stack = new Stack<>();
 
-        Stack<Integer> stack = new Stack<>();
+		int nextPush = 1;
 
-        int num = 1; // 수열의 시작
-        boolean result = true;
-
-        StringBuffer bf = new StringBuffer(); // 나중에 출력할 스택 현황
+		for(int i = 0; i < N; i++) { // [3, 1, 2]
+			int target = Integer.parseInt(br.readLine());
 
 
-        for(int i = 0; i < N; i++) {
-            int su = A[i];
-            if (su >= num) {
-                while (su >= num) {
-                    stack.push(num++);
-                    bf.append("+\n");
-                }
-                stack.pop();
-                bf.append("-\n");
-            } else { // 수열에 있는 수가 스택안에 있는 숫자보다 작을때,
-                int n = stack.pop();
-                if (n > su) { // 스택에 있는 수가 수열안에 있는 수보다 크면
-                    System.out.println("NO");
-                    result = false;
-                    break;
-                } else {
-                    bf.append("-\n");
-                }
-            }
-        }
-        if(result ) {
-            System.out.println(bf);
-        }
+			while(nextPush <= target) {
 
-    }
+				stack.push(nextPush);
+				sb.append("+\n");
+				nextPush++; // 3
+			}
+
+			if(!stack.isEmpty() && stack.peek() == target) {
+				stack.pop();
+				sb.append("-\n");
+			} else {
+				System.out.println("NO");
+				return;
+			}
+		}
+		System.out.println(sb);
+	}
+
 }
-

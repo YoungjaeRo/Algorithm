279b47d916295be16841a6eeb493baa710bf37fc
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-19 00:28:07 +0900

    [Silver III] Title: N과 M (3), Time: 376 ms, Memory: 68420 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,44 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 
 public class Main {
-	static int N; // 1 ~ N까지의 자연수
-	static int M; // M개를 고름 그중
-	static int[] arr; // 답을 저장해놓을 배열
-	static StringBuilder sb  = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception {
+	static int N;
+	static int M;
+
+	static int[] arr; // 뽑은 숫자들을 저장해놓을 배열
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+
 		arr = new int[M];
-		
+
 		dfs(0);
+
 		System.out.println(sb);
+
 	}
-	
-	
-	
-	
-	static void dfs(int depth) {
-		
-		if(depth == M) { // 종료 조건
-			for(int i = 0; i < M; i++) {
-				sb.append(arr[i]).append(" ");
+
+	static void dfs(int depth) { // 0-- >
+
+		//1. 종료 조건 부터
+		if(depth == M) {
+			for(int i : arr) {
+				sb.append(i).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
-		
-		for(int i = 1; i <= N; i++) {
-			
+
+		for(int i = 1; i <= N; i++) {// 0
 			// 같은 숫자를 쓰는 경우
 			arr[depth] = i;
-			dfs(depth + 1);
+			dfs(depth + 1); // dfs(0) --
 		}
 	}
 }

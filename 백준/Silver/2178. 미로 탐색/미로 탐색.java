f3aebfa92057659263b2143765e1ff2774a78e9b
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-20 19:33:02 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,69 +1,69 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int N;
+
 	static int M;
-
-	static int[][] map;
-	static int[][] dist; // 해당 칸까지의 최단 거리
-
-	static boolean[][] visited;
 
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int [][] map;
+	static boolean[][] visited;
+	static int[][] dist;
 
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		map = new int[N][M];
-		dist = new int[N][M];
+		map = new int [N][M];
 
 		visited = new boolean[N][M];
 
-		// 그래프에 숫자 정보 입력함
-		for(int i = 0; i < N; i++) {
-			String numbers = br.readLine();
 
-			for(int j = 0; j < M; j++) {
-				map[i][j] = numbers.charAt(j) - '0';
+		dist = new int[N][M];
+
+		dist[0][0] = 1;
+
+		for(int i = 0; i < N; i++) {
+			String line = br.readLine();
+
+			for(int j = 0;  j < M; j++) {
+				map[i][j] = line.charAt(j) - '0';
 			}
 		}
-
 		bfs(0, 0);
 
-		System.out.println(dist[N-1][M-1]);
+		System.out.println(dist[N -1][M-1]);
+
 	}
 
-	static void bfs(int x, int y) {
+	static void bfs(int r, int c) {
 		ArrayDeque<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {r, c});
 
-		visited[x][y] = true;
-		dist[x][y] = 1;
-
-		q.offer(new int[]{x, y});
+		visited[r][c] = true;
 
 		while(!q.isEmpty()) {
 			int[] cur = q.poll();
-			int curX = cur[0];
-			int curY = cur[1];
+
+			int cr = cur[0];
+			int cc = cur[1];
 
 			for(int i = 0; i < 4; i++) {
-				int nX = curX + dx[i];
-				int nY = curY + dy[i];
+				int nr = cr + dx[i];
+				int nc = cc + dy[i];
 
-				if(nX < 0 || nX >= N || nY < 0 || nY >= M) {
-					continue;
-				}
+				if(nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && map[nr][nc] == 1) {
 
-				if(visited[nX][nY] == false && map[nX][nY] != 0) {
-					dist[nX][nY] = dist[curX][curY] + 1;
-					visited[nX][nY] = true;
-					q.offer(new int[] {nX, nY});
+					dist[nr][nc] = dist[cr][cc] + 1;
+					visited[nr][nc] = true;
+
+					q.offer(new int[] {nr, nc});
 				}
 			}
 		}

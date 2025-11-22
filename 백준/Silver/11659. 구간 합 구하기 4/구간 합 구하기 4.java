e95f0aa6695609dcd8f8c8ba82d489ff1a73dfd5
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-11-22 13:24:28 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 552 ms, Memory: 52720 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,50 +1,45 @@
-// 일일이 계산하면 너무 오래걸리기 때문에, 누적합(prefix sum)을 활용해서 문제를 해결
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-
-/**
- * prefix[i] = A[1] + A[2] + ... + A[i]
- * 즉, 처음부터 i번째까지의 합을 미리 저장해 두는 배열
- * 관례상 prefix[0] = 0으로 둡니다 (인덱스 편하려고)
- */
 public class Main {
-
 	static int N;
 	static int M;
-	static long[] prefix; // 누적합 배열 0은 0번으로 비워두기 (인덱스 편리용)
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		/**
+		 * 수 N까지 주어졌을때, i번째부터 j까지수의 합을 구하시오
+		 * 누적합 배열을 만들어야함 prefix[i] = prefix[i-1] + arr[i];
+		 */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		prefix = new long[N + 1];
-		prefix[0] = 0;
-
-		
-		//  누적합 채우기
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			int x = Integer.parseInt(st.nextToken());
-			prefix[i + 1] = prefix[i] + x;
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		// 누적합 배열
+		int[] prefix = new int[N + 1]; // (1-base) 둔다.
+
+		st = new StringTokenizer(br.readLine());
+
+		// 누적합 배열 선언 완료
+
+		for(int i = 1; i <= N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			prefix[i] = prefix[i-1] + num;
 		}
 
-		// 질의처리
 		StringBuilder sb = new StringBuilder();
-		for(int j = 0; j < M; j++) {
+
+		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 
-			long sum = prefix[end] - prefix[start - 1];
-			sb.append(sum).append("\n");
+			int answer = prefix[end] - prefix[start - 1];
+			sb.append(answer).append("\n");
 		}
-
 		System.out.println(sb.toString());
 
 	}
-
 }

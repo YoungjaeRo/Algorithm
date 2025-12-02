e1d603d50daffdbd90d6ed913ed0de906e26d260
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-12-02 19:43:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,52 +1,56 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+
+
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
 	/**
-	 * 점화식 : dp[i] --> i번째 원소로 끝나는 증가 부분수열의 최대 길이
-	 * dp[i] : 1 + Math.max(dp[j]) 단, i > j, 그리고 A[i] > A[j]
-	 * 붙일게 없다면, dp[i] = A[i] 자기 자신만을 고른 경우 : 길이 1
+	 * 해당 문제를 dp로 푼다고 할때 점화식을 어떻게 세우는 게 좋을까
+	 * dp[i] = i가 가장 마지막 순서인 길이
 	 */
+
+	static int A;
+	static int[] arr ; // 숫자 저장용 배열
+	static int[] dp; // dp 용 배열임
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-
-		int[] A = new int[N]; // 각 배열의 위치와 숫자를 저장할 배열 생성
+		A = Integer.parseInt(br.readLine());
+		arr = new int[A];
+		dp = new int[A];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for(int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-
+		for(int i = 0; i < A; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
 		/**
-		 * dp[i] = i번째 원소 A[i]로 끝나는 증가 부분수열의 최대 길이
+		 * dp 시작
 		 */
 
-		int[] dp = new int[N];
-		Arrays.fill(dp, 1); // 최소 자기 자신 : 길이 1
+		// 1. 일단은 자기자신(길이 1)로 초기화 해줌
 		
-
-		int answer = 1;
-
-		for (int i = 0; i < N; i++) {
+		Arrays.fill(dp, 1);
+		
+		for(int i = 0; i < dp.length; i++) {
 			for(int j = 0; j < i; j++) {
 				
-				// 증가 조건을 충족한다면,
-				if(A[j] < A[i]) {
-
+				if(arr[j] < arr[i]) { // 수열 증가 조건을 충족한다면,
 					dp[i] = Math.max(dp[i], dp[j] + 1);
 				}
 			}
+		}
 
-			// 전체 최댓값 갱신
-			answer = Math.max(answer, dp[i]);
+		int answer = 1;
+		
+		for(int num : dp) {
+			if(num > answer) {
+				answer = num;
+			}
 		}
 		System.out.println(answer);
 	}
 }
-

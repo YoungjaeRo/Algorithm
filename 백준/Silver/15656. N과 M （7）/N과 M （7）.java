e9425b3aafc29d979b24b6d2d9259d3031055aad
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-12-09 10:43:16 +0900

    [Silver III] Title: N과 M (7), Time: 560 ms, Memory: 123668 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -4,8 +4,14 @@ import java.util.*;
 public class Main {
 	/**
 	 * 같은 수를 여러번 골라도 되고, 길이가 M (depth == M)
-	 * 순서가 중요한 수열문제이기 때문에, visited 배열이 필요함
-	 * 중복해서 선택이 가능한 수열인데, 과연 visited가 필요하나,,,
+	 * 중복을 허용하기 때문에, visited 배열은 필요가 없음
+	 * 해당 문제는 수열 문제이기 때문에, start또한 필요없다.
+	 * 
+	 * 수열 (중복 허용) : visited 배열 필요없음, only depth만
+	 * 수열 (중복 X) : visited 배열 필요하고, 다음 dfs 이후 꼭 원복을 시행해줘야한다
+	 * 
+	 * 조합 (중복허용) : depth와 start 인자가 필요함, start를 다음 dfs에서 증가시키지 않음
+	 * 조합 (중복 비허용) : depth와 start 인자가 필요함, start를 다음 dfs에서 증가시킴
 	 */
 	static int N;
 	static int M;
@@ -13,8 +19,6 @@ public class Main {
 	static int[] arr;
 
 	static int[] answer;
-
-	static boolean[] visited;
 
 	static StringBuilder sb = new StringBuilder();
 
@@ -39,12 +43,12 @@ public class Main {
 		// 사전순으로 증가하는 순서로 출력해야하기 때문에, 오름차순 정렬을 해준다
 		Arrays.sort(arr);
 
-		backtrack(0, 0); // depth = 0, 시작 인덱스 0
+		backtrack(0); // depth = 0, 시작 인덱스 0
 
 		System.out.println(sb.toString());
 	}
 
-	static void backtrack(int depth, int start) {
+	static void backtrack(int depth) {
 		if(depth == M) {
 			for(int i : answer) {
 				sb.append(i).append(" ");
@@ -53,10 +57,10 @@ public class Main {
 			return;
 		}
 
-		for(int i = start; i < N; i++) {
+		for(int i = 0; i < N; i++) {
 			answer[depth] = arr[i];
 
-			backtrack(depth + 1, start);
+			backtrack(depth + 1);
 		}
 	}
 

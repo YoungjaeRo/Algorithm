8af1b9e380e5a3cb7e3659493ea883720dc64611
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-12-12 22:23:13 +0900

    [Silver III] Title: N과 M (1), Time: 252 ms, Memory: 23588 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,55 +1,57 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
+
 	static int N;
 	static int M;
-	
-	static int[] arr; // 뽑은 숫자들을 저장할 배열 (길이 M)
-	
-	static boolean[] visited; // 숫자 사용 여부 체크 (1 ~ N)
-	
-	public static void main(String[] args) throws Exception {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 StringTokenizer st = new StringTokenizer(br.readLine());
 
-		        N = Integer.parseInt(st.nextToken());
-		        M = Integer.parseInt(st.nextToken());
-		        
-		        arr = new int[M];
-		        
-		        visited = new boolean[N + 1]; // 1 ~ N까지 쓰기 위해서        
-		        
-		        dfs(0);
+	static int[] arr;
+	static boolean[] visited;
+
+	static int[] answer;
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		visited = new boolean[N];
+
+		answer = new int[M];
+
+		for(int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+
+		Arrays.sort(arr);
+
+		backtrack(0);
+		System.out.println(sb);
 	}
-	
-	static void dfs(int depth) {
-		if(depth == M) { // 숫자를 다 사용했으면,
-			
-			for(int i = 0; i < M; i++) {
-				System.out.print(arr[i] + " ");
+
+	static void backtrack(int depth) {
+		if(depth == M) {
+			for(int num : answer) {
+				sb.append(num).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
 		}
-		
-		
-		for(int i = 1; i <= N; i++) { // 1 ~ N까지 숫자 중에서 이번 자리(depth)에 올 숫자 고르기
-			if(visited[i]) {
-				continue;
+
+		for(int i = 0; i < N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				answer[depth] = arr[i];
+
+				backtrack(depth + 1);
+				visited[i] = false;
 			}
-			
-			//i 를 이번에 사용할 예정
-			visited[i] = true;
-			arr[depth] = i;
-			
-			// 다음 자리 수 채우러 감
-			dfs(depth + 1);
-			visited[i] = false; // 백트래킹
-			
 		}
 	}
-	
-
 }

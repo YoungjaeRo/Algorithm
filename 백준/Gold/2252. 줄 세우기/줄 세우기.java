f2a34327b287a97823a07e36314ee7130e25ffa6
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-12-22 22:17:02 +0900

    [Gold III] Title: 줄 세우기, Time: 408 ms, Memory: 44188 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,55 +1,79 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main { // 정답이 여러가지 일 경우, 그냥 그 답을 출력? --> 답이 여러개가 나올 수 있음 --> 위상정렬 알고리즘
-	public static void main(String[] args) {
-		// 1. 위상정렬 알고리즘을 사용할 것이므로, 그래프 데이터를 바탕으로 인접리스트 (ArrayList<>[]를 생성함) & 진입차수 배열도 생성함
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt(); // 학생수
-		int M = scanner.nextInt(); // 비교 횟수
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		// 인접리스트 초기화
-		ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-		for(int i = 0; i<= N; i++) {
-			A.add(new ArrayList<>());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		// 위상 정렬은 무.조.건 인접리스트로 진입차수로 표현한다
+		ArrayList<Integer>[] graph = new ArrayList[N + 1];
+
+		// graph[A] = A 다음에 와야하는 학생들 목록
+
+		for(int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
 		}
 
-		// 진입차수 배열 생성
+		// indegree[i] = 진입차수 배열(꼭 필수임)
 		int[] indegree = new int[N + 1];
 
-        // 인접리스트에 데이터 저장하기
+		// 1. 비교 정보 입력
 		for(int i = 0; i < M; i++) {
-			int start = scanner.nextInt();
-			int end = scanner.nextInt();
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
 
-			A.get(start).add(end); // 해당 인접리스트에 데이터 저장하기 ex 4 --> 2
-			indegree[end]++; // 4가 2를 가리키므로, 2의 진입차수 배열에 1 추가
+			//A가 B보다 앞에 서야한다 --> 기준 방향으로 입력하기
+			graph[A].add(B);
+
+			// 진입차수 배열도 업데이트
+			indegree[B]++;
 		}
 
-		// 위상정렬 시작
-		// 1. 큐를 만들어주기
-		Queue<Integer> queue = new LinkedList<>();
+		// 2.진입차수가 0인 학생들을 담을 큐
+		// 지금 당장 줄세워도 되는 것들
+		Queue<Integer> q = new ArrayDeque<>();
 
-		// 2. 진입차수 탐색
-		for(int i = 1; i<= N; i++) {
-			if(indegree[i] == 0) { // 진입차수가 0인 노드를 큐에 삽입함
-				queue.offer(i);
+		// 3. 처음엔 진입차수가 0인애들을 큐에 넣기
+		for(int i = 1; i <= N; i++) {
+			if(indegree[i] == 0) {
+				q.offer(i);
 			}
 		}
 
-		// 3. 큐에 아무것도 없을때까지 실행함
-		while (!queue.isEmpty()) {
-			int now = queue.poll(); // 현재 큐에 담겨있는 노드를 빼서 정답으로 출력함
-			System.out.println(now + " ");
-			for(int next : A.get(now)) {
+		// 결과를 저장할 StringBuilder
+
+		StringBuilder sb = new StringBuilder();
+
+		// 4. 위상정렬 시작
+		while(!q.isEmpty()) {
+
+			// 현재 줄에 세울 학생
+			int cur = q.poll();
+			sb.append(cur).append(" ");
+
+			// cur 다음 와야할 학생들 처리
+			for(int next : graph[cur]) {
+
+				// cur을 앞에 세웠으므로, next의 진입차수 1 감소
 				indegree[next]--;
+
+				// 이제 앞에 올 사람이 없어졌다면 큐에 추가
 				if(indegree[next] == 0) {
-					queue.offer(next);
+					q.offer(next);
 				}
 			}
 		}
+
+		System.out.println(sb.toString());
+
+
+
+
 
 	}
 }

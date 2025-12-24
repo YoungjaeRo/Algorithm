fb1899428881d2728e15313f2131dcc3530da627
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-12-24 17:45:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 532 ms, Memory: 50728 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,67 +1,100 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	static int V, E;
-	static int[] parent;
-	static Edge[] edges;
-	static int totalWeight;
+	static int V; // 정점의 개수
+	static int E; // 간선의 수
 
-	static class Edge {
-		int u, v, weight;
 
-		public Edge(int u, int v, int weight) {
-			this.u = u;
-			this.v = v;
-			this.weight = weight;
+	static class Edge implements Comparable<Edge> {
+		int a;
+		int b;
+		int cost;
+
+		Edge(int a, int b, int cost) {
+			this.a = a;
+			this.b = b;
+			this.cost = cost;
+		}
+
+		// 간선 비용 기준 오름차순 정렬
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.cost, o.cost);
 		}
 	}
 
-	// Find 연산 (경로압축)
+	static int[] parents;
+
 	static int find(int x) {
-		if (parent[x] == x) return x;
-		return parent[x] = find(parent[x]);
+		if(x == parents[x]) {
+			return x;
+		}
+		// 경로 압축
+		parents[x] = find(parents[x]);
+
+		return parents[x];
 	}
 
-	// Union 연산
-	static void union(int a, int b) {
-		int rootA = find(a);
-		int rootB = find(b);
-		if (rootA != rootB) parent[rootB] = rootA;
+	static boolean union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+
+		if(pa == pb) {
+			// 둘이 같은 그룹이면 false 반환
+			return false;
+		}
+
+		// 한쪽으로 부모노트 통일하기
+		parents[pb] = pa;
+		return true;
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 
-		parent = new int[V + 1];
-		edges = new Edge[E];
+		// MST는 꼭 엣지리스트로 문제를 풀어야 한다
+		List<Edge> edges = new ArrayList<>();
 
-		for (int i = 1; i <= V; i++) {
-			parent[i] = i;
-		}
-
-		for (int i = 0; i < E; i++) {
+		// 간선 정보 입력
+		for(int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-
-			edges[i] = new Edge(u, v, w);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			edges.add(new Edge(a, b, cost));
 		}
 
-		Arrays.sort(edges, Comparator.comparingInt(e -> e.weight));
+		// 오름차순 정렬 (필수)
+		Collections.sort(edges);
 
-		for (Edge edge : edges) {
-			if (find(edge.u) != find(edge.v)) {
-				union(edge.u, edge.v);
-				totalWeight += edge.weight;
+		// 유니온 파인드 초기화
+		parents = new int[V + 1];
+		for(int i = 1; i <= V; i++) {
+			parents[i] = i;
+		}
+		
+		int answer = 0;
+		int cnt = 0;  // 딱 노드 -1까지만 허용
+		
+		for(Edge e : edges) {
+			// 둘이 다른 그룹이라면 --> 이어주고 MTS에 추가
+			if(union(e.a, e.b)) {
+				answer = answer + e.cost;
+				cnt++;
+			}
+			
+			if(cnt == V -1) {
+				break;
 			}
 		}
 
-		System.out.println(totalWeight);
+		System.out.println(answer);
+
 	}
 }

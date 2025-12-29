5d9cd0f05ed9b9d68d01486a9d0b92cc6e0a571b
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-12-29 15:54:21 +0900

    [level 2] Title: 올바른 괄호, Time: 17.45 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,25 +3,30 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
+Stack<Character> stack = new Stack<>();
 
-		Stack<Character> stack = new Stack<>();
+		//문자열의 길이 -1  만큼 루프
+		int length = s.length();
 
-		for(int i = 0; i < s.length(); i++) {
+		for(int i = 0; i < length; i++) {
 			char c = s.charAt(i);
 
+			// 열린 괄호라면, 스택에 삽입
 			if(c == '(') {
 				stack.push(c);
-			}
-
-			else { // 닫는 괄호일때,
+			} else { // 닫는 괄호이면, pop 대신 스택 자체가 비어있으면('(') 이 없으면 바로 false 출력
 				if(stack.isEmpty()) {
 					return false;
 				}
 
-				stack.pop(); // 정상적으로 짝을 맞춰서 하나를 꺼냄
+				stack.pop();
 			}
-
 		}
-		return stack.isEmpty();
+
+		// 스택이 비어있으면 완벽한 문장, 아니면, 비완벽
+		boolean answer = stack.isEmpty();
+
+		return answer;
+		
 	}
 }

34583eddc854b42c783ec3fc16939148c5f03703
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-01-15 14:40:59 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,48 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	/**
+	 * 1,2,3을 더해서 정수 4를 나타내는 법
+	 *
+	 * dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3]
+	 */
 
-	static int T;
-	static int[] arr;
-	static int[] dp;
+	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String args[]) throws Exception {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 
-	T = Integer.parseInt(br.readLine());
+		while(T-- > 0) {
+			int n = Integer.parseInt(br.readLine());
 
-	arr = new int[T];
+			int[] dp = new int[n + 1];
 
-	dp = new int[12]; // 정수는 최대 11까지 주어짐
+			dp[0] = 0;
+			if (n >= 1) dp[1] = 1;
+			if (n >= 2) dp[2] = 2;
+			if (n >= 3) dp[3] = 4;
 
-	for(int i = 0; i < T; i++) {
-		int num = Integer.parseInt(br.readLine());
-		arr[i] = num;
-	}
 
-		/**
-		 * dp[i]는 1,2,3으로 i를 만들 수 있는 경우의 수
-		 * dp[i] = dp[i -1] + dp[i-2] + dp[i-3]
-		 */
+			for(int i = 4; i <= n; i++) {
+				dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+			}
 
-		// 기저값들 세팅하기
-		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] =4;
-
-		for(int i = 4; i <= 11; i++) {
-			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+			sb.append(dp[n]).append("\n");
 		}
 
-		//각 테스트 케이스마다 결과 출력하기
-		for(int num : arr) {
-			sb.append(dp[num]).append("\n");
-		}
-
-		System.out.println(sb);
-
+		System.out.println(sb.toString());
 	}
 }

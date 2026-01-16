f188249afeb7617ad4f41bc5fd0fcdfe78e303ef
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-01-16 15:31:12 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 23580 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,49 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static final int max = 100001;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	static final int MAX = 100001;
 
+	public static void main(String[] args) throws Exception {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+	int N = Integer.parseInt(st.nextToken());
+	int K = Integer.parseInt(st.nextToken());
 
-		int[] time = new int[max];
-		boolean[] visitied = new boolean[max];
+	int[] time = new int[MAX];
+	boolean[] visited = new boolean[MAX];
 
+	Queue<Integer> q = new LinkedList<>();
+	visited[N] = true;
+	time[N] = 0;
 
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(N);
-		visitied[N] = true;
-		time[N] = 0;
-		
-		while(!q.isEmpty()) {
-			int current = q.poll();
-			
-			if(current == K) {
-				System.out.println(time[current]);
-				break;
+	q.offer(N);
+
+	while(!q.isEmpty()) {
+		int cur = q.poll();
+
+		if(cur == K) {
+			System.out.println(time[cur]);
+			break;
+		}
+
+		int[] options = {cur - 1, cur + 1, cur * 2};
+
+		for(int next : options) {
+
+			// 1. 범위 밖이라면
+			if(next < 0 || next >= MAX) {
+				continue;
 			}
+
+			// 2. 이미 방문을 했다면,
+			if(visited[next]) {
+				continue;
+			}
+
+			// 3. 방문 처리
+			visited[next] = true;
 			
-			int[] nextPositions = {current -1, current + 1, current * 2};
+			// 4. 시간 업데이트
+			time[next] = time[cur] + 1;
 			
-			for(int next : nextPositions) {
-				if(next >= 0 && next < max && !visitied[next]) {
-					visitied[next] = true;
-					time[next] = time[current] + 1;
-					q.offer(next);
-				}
+			// 5. 큐 삽입
+			q.offer(next);
 			}
 		}
-			
 	}
-
 }

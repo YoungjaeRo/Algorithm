0f03295f44bedb095bd459aeee926938ef908639
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-01-30 12:33:50 +0900

    [Silver IV] Title: 로프, Time: 340 ms, Memory: 28364 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,38 +1,45 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	/**
+	 * 각기 다르게 버틸 수 있는 무게가 다른 로프들이 있다.
+	 * 이 로프들을 활용하여 들어올릴 수 있는 물체의 최대 중량을 구하시오
+	 *
+	 * 로프들이 들어올릴 수 있는 최대 중량
+	 * 20, 10 이면 최대 버틸 수 있는 무게는 10 * 20  = 20
+	 */
+	static int N;
+
+	static Integer[] weights;
+
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		// 로프 알고리즘:
-		// 예시: 루프들이 최대한 들어올릴 수 있는 중량
-		// 26, 10 두 개의 로프를 사용하면 10*2 = 20 이 버틸 수 있는 최대 중량이다.
+		N = Integer.parseInt(br.readLine());
 
-		// 1. 30 하나만 사용할 때 30
-		// 2. 30, 26 두개 사용할 때 52
-		// 3. 30, 26, 10 세개 사용할 때 30
-		// 4. 30, 26, 10, 5 모두 사용할 때 20
+		weights = new Integer[N];
 
-		// 즉, 중량이 큰 로프부터 하나씩 꺼내서 계산후, 최댓값을 구하는게 좋다
-
-		// 로프의 개수
-		int N = Integer.parseInt(br.readLine());
-
-		// 각 무게들을 저장할 배열을 생성
-		Integer[] ropes = new Integer[N];
-
-		for(int i = 0; i < N; i++) { // 무게들을 입력받고 저장하기
-			ropes[i] = Integer.parseInt(br.readLine());
+		for(int i = 0; i < N; i++) {
+			int weight = Integer.parseInt(br.readLine());
+			weights[i] = weight;
 		}
-		// 내림차순으로 정렬하기
-		Arrays.sort(ropes, Collections.reverseOrder());
-		int total = 0;
-		for(int i = 0; i < N; i++){
-			total = Math.max(total, ropes[i] * (i + 1));
+
+		// 내림차순 : 강한 로프부터 하나씩 추가해보는 게 직관적
+		Arrays.sort(weights, Collections.reverseOrder());
+
+		long answer = 0;
+
+		for(int i = 0; i < N; i++) {
+			long weakest = weights[i]; // 현재 선택한 로프중 가장 최약
+			long pick = i + 1; // 선택한 로프 개수
+			long canLift = weakest * pick;
+
+			answer = Math.max(answer, canLift);
 		}
-		System.out.println(total);
+
+		System.out.println(answer);
 	}
 }

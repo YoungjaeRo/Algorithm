35aa6faf6dca3974223a158239bafe7aa978d193
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-02-14 17:07:07 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -2,55 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+	static int N; // 수열의 크기
+
 	/**
-	 * 해당 문제를 dp라고 생각했을때, dp[i] = i까지 포함된, 증가하는 수열의 가장 큰값
-	 *
+	 * 증가하는 부분 수열 중 합이 가장 큰 것
+	 * dp[i] = i 번째 원소를 마지막으로 했을때, 얻을 수 있는 최댓값
 	 */
 
-	static int A; // 수열의 크기
-	static int[] arr; // 숫자를 저장해놓을 배열
-	static int[] dp; // dp전용 배열
-
+	static int[] arr;
+	static int[] dp;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		A = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
-		arr = new int[A];
-		dp = new int[A];
+		dp = new int[N + 1];
+		arr = new int[N + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		// 배열에 숫자 정보 저장
-		for(int i = 0; i < A; i++) {
+		for(int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		/**
-		 * dp[i] = i까지 포함한, 증가하는 부분수열의 최대값
-		 */
+		int ans = 0;
 
-		for(int i = 0; i < A; i++) {
+		// dp 시작
+		for(int i = 1; i <= N; i++) {
 
-			//일단은 본인의 arr[i]에 대응하는 값들로 세팅해준다
-			dp[i] = arr[i];
+			dp[i] = arr[i]; // 각자 본인으로 초기화
 
-			for(int j = 0; j < i; j++) {
-				if(arr[j] < arr[i]) { // 앞에 있는 값보다 더 크다면 --> 증가하는 수열의 조건
+			for(int j = 1; j < i; j++) {
+
+				// 증가 수열 조건과 맞는다면,
+				if(arr[j] < arr[i]) {
 					dp[i] = Math.max(dp[i], dp[j] + arr[i]);
 				}
 			}
+
+			ans = Math.max(ans, dp[i]);
 		}
 
-		int answer = 0;
-		for(int i = 0; i < A; i++) {
-			if(dp[i] > answer) {
-				answer = dp[i];
-			}
-		}
-
-		System.out.println(answer);
-
+		System.out.println(ans);
 	}
 }

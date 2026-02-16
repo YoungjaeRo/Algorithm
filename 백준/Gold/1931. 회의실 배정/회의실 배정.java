2f2d1738e78315292201c3b1be1aede6a5dace2b
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-02-16 20:31:13 +0900

    [Gold V] Title: 회의실 배정, Time: 416 ms, Memory: 45548 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,60 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	/**
-	 * N개의 회의에 대해서, 회의실 사용표를 만들려고 한다
-	 *
-	 * 각 회의 I에 대해서, 시작시간과 끝나는 시간이 주어져 있고,
-	 *
-	 * 각 회의가 겹치지 않게 하면서, 회의실을 사용할 수 있는 화의의 최대 개수를 구하여라
-	 *
-	 * 회의는 한번 시작하면 중단할 수 없으며,
-	 *
-	 * 끝나는 시간이 가장 빠른 회의부터 선택한다
-	 *
-	 */
 
-	public static void main(String[] args)  throws Exception {
+	static class Meeting implements Comparable<Meeting>{
+		int start;
+		int end;
+
+		Meeting(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public int compareTo(Meeting o) {
+			if(this.end != o.end) {
+				return this.end - o.end;
+			} else {
+				return this.start - o.start;
+			}
+		}
+	}
+
+	static int N;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 
-		int[][] meetings = new int[N][2];
+		N = Integer.parseInt(br.readLine());
+
+		// 정렬기준으로 정렬된 회의들을 저장해놓을 리스트
+		List<Meeting> meetings = new ArrayList<>();
 
 		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 
-			meetings[i][0] = Integer.parseInt(st.nextToken()); // 시작
-			meetings[i][1] = Integer.parseInt(st.nextToken()); // 종료
-
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			meetings.add(new Meeting(start, end));
 		}
 
-		// 빨리 끝나는 시간 순으로 정렬, 같으면, 빨리 시작하는 순으로 정렬하기
-		Arrays.sort(meetings, new Comparator<int[]>() {
+		// 종료시간이 빠른것부터 정렬하기 ㅇㅇ
+		Collections.sort(meetings);
 
-			// 외부에서 정렬기준을 정할땐, new Comparator<>() {}를 해주고,
-			//  오버라이딩은 compareTo가 아닌 compare이다 ㅇㅇ
-			@Override
-			public int compare(int[] a, int[] b) {
-				if(a[1] != b[1]) {
-					return a[1] - b[1];
-				} else {
-					return a[0] - b[0];
-				}
-			}
-		});
+		int ans = 0;
 
-		int answer = 0; // 회의의 수
+		int lastEnd = 0;
 
-		int endTime = 0; // 마지막으로 진행된 회의의 종료시간
-		
-		for(int i = 0; i < N; i++) {
-			// 지금 회의 시작 시간이 끝나는 시간보다 이상이면, 회의 시작가능
-			if(meetings[i][0] >= endTime) {
-				answer++;
-				endTime = meetings[i][1];
+		for(Meeting cur : meetings) {
+		// 이번 회의 시작 시간이, 이전 회의의 종료 시간 이후라면, 겹치지 않는다
+			if(cur.start >= lastEnd) {
+				ans++;
+				lastEnd = cur.end;
 			}
 		}
-		System.out.println(answer);
 
+		System.out.println(ans);
 	}
 }

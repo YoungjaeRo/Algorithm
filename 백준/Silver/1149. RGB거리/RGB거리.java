2e0c9b83f796f94092fddc7f8cc4a70d7e2fb5c7
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-05 16:41:26 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,40 +1,52 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	static int N;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
-		// 집의 수 N
-		int N = Integer.parseInt(br.readLine());
+		int[][] colors = new int[N + 1][3];
+		int[][] dp = new int[N + 1][3];
 
-		// cost[i][j] = i 번째 집을 j색으로 칠할 때의 비용
-		int[][] cost = new int[N][3];
-
-		// 입력 받기
-		for (int i = 0; i < N; i++) {
+		// 색깔 정보 입력 받기
+		for(int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			cost[i][0] = Integer.parseInt(st.nextToken()); // 빨강
-			cost[i][1] = Integer.parseInt(st.nextToken()); // 초록
-			cost[i][2] = Integer.parseInt(st.nextToken()); // 파랑
+			colors[i][0] = Integer.parseInt(st.nextToken()); // 빨강
+			colors[i][1] = Integer.parseInt(st.nextToken()); // 초록
+			colors[i][2] = Integer.parseInt(st.nextToken()); // 파랑
 		}
 
-		// DP 배열 : dp[i][j] = i 번째 집까지 칠할때, j 색으로 끝나는 최소 비용
-		int[][] dp = new int[N][3];
+		/**
+		 * dp[i][0~2] : i번째 집에 0 ~ 2색을 칠할때의 최소 비용
+		 */
 
-		// 초기값: 첫 번째 집은 그냥 칠하는 비용 그대로
-		dp[0][0] = cost[0][0];
-		dp[0][1] = cost[0][1];
-		dp[0][2] = cost[0][2];
+		// 기저값 세팅하기 (첫번째 집을 해당 색깔로 칠할때의 최소 비용)
+		dp[1][0] = colors[1][0];
+		dp[1][1] = colors[1][1];
+		dp[1][2] = colors[1][2];
 
-		// 두번째 집부터 DP 채우기
-		for(int i = 1; i < N; i++) {
-			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0]; // 전에는 초록 또는 파란 현재는 "빨강"
-			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1]; // 전에는 빨강 또는 파란 현재는 "초록"
-			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2]; // 전에는 초록 또는 빨강 현재는 "파란"
+		// dp 진행
+		for(int i = 2; i <= N; i++) {
+			// i번째 집을 빨강으로 색칠할 경우, 바로 전 집은 초록 또는 파란색이어야 한다
+			dp[i][0] = colors[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+
+			// i번째 집을 초록색으로 색칠할 경우,
+			dp[i][1] = colors[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+
+			// i번째 집을 파란색으로 색칠할 경우
+			dp[i][2] = colors[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+
 		}
 
-		// 마지막 집까지 칠한 뒤, 가장 싼 경우 출력
-		int answer = Math.min(Math.min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]);
+		int answer = Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2]));
+
+
 		System.out.println(answer);
+
+
 	}
 }

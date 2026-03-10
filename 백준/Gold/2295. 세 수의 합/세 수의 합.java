e624d39bc44a5eef6285b75f70b45d4806339eb6
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-10 18:52:05 +0900

    [Gold IV] Title: 세 수의 합, Time: 644 ms, Memory: 53512 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -2,87 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	/**
-	 * 입력 배열 U를 정렬한다
-	 * 모든 a + b 값을 twoSum에 넣는다
-	 * twoSum을 정렬한다
+	 * a + b + c = d (U안에 d도 포함되어 있음)
+	 * a + b = d - c
 	 *
+	 * 결국 a,b 두 수의 합에서 c를 뺀 값이 target이고, 해당 d가 그러한 값이 있는지를 판별하면 되는 문제이다.
 	 */
+
+	static int N; // 숫자의 개수
+
+	static int[] arr; // 숫자 저장할 배열
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		int [] U = new int[N];
+		N = Integer.parseInt(br.readLine());
 
+		arr = new int[N];
 		for(int i = 0; i < N; i++) {
-			U[i] = Integer.parseInt(br.readLine());
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		// 1. U[i] 정렬하기, 나중에 d를 큰것부터 보기 위해서
-		Arrays.sort(U);
+		// 오름차순 정렬해주기
+		Arrays.sort(arr);
 
-		// 2. twoSum = 가능한 모든 (a + b) 값 저장
-		int size = N * N;
-		int[] twoSums = new int[size];
-
-		int idx = 0;
+		// 두 수의 합을 저장해놓을 리스트
+		ArrayList<Integer> twoSum = new ArrayList<>();
 
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				twoSums[idx++] = U[i] + U[j];
+				twoSum.add(arr[i] + arr[j]);
 			}
 		}
 
-		// 3. 이진 탐색을 위한 정렬
-		Arrays.sort(twoSums);
+		// 이분 탐색을 위한 정렬
+		Collections.sort(twoSum);
 
-		// 4) d를 큰 값부터 내려가면서 확인
-		//    d = U[i]
-		//    d - c 가 twoSums 안에 있으면 d = a+b+c 성립
+		// 큰 수부터 보면서, 이분 탐색 시작하기
+		for(int i = N - 1; i >= 0; i--) {
+			int d = arr[i];
 
-		for(int i = N- 1; i >= 0; i--) { // d 후보를 큰것부터 탐색
-			int d = U[i];
-
-			// 모든 c에 대해 target = d - c
 			for(int j = 0; j < N; j++) {
-				int c = U[j];
+				int target = d - arr[j];
 
-				int target = d - c;
-
-				if(binarySearch(twoSums, target)) {
-					// 큰것부터 탐색했으니 해당 값이 가장 큰값
+				if(binarySearch(twoSum,target)) {
 					System.out.println(d);
-                    return;
+					return;
 				}
 			}
-
 		}
 	}
 
-	static boolean binarySearch(int[] sum, int target) {
+	static boolean binarySearch(List<Integer> list, int target) {
 		int left = 0;
-		int right = sum.length - 1;
+		int right = list.size() - 1;
 
 		while(left <= right) {
 			int mid = (left + right) / 2;
 
-			// 찾은 경우
-			if(sum[mid] == target) {
-				return true;
-			}
+			int value = list.get(mid);
 
-			// target이 더 큰 경우
-			if(sum[mid] < target) {
+			if(value == target) {
+				return true; // 찾음
+			} else if(value < target) {
 				left = mid + 1;
-			}
-			// target이 더 작은 경우
-			else {
+			} else {
 				right = mid - 1;
 			}
 		}
-		
-		// 끝까지 못찾았으면
+
+		// 그래도 못찾았으면 false
 		return false;
 	}
 }

9b42bf66b73a997401c26d26e559cb1143cfcefb
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-15 15:50:30 +0900

    [Silver II] Title: 과자 나눠주기, Time: 684 ms, Memory: 115304 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -2,60 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	/**
-	 *  홍익이는 조카들에게 최대한 긴 과자를 나누어주려고 한다
-	 *
-	 * 	하지만, 나눠준 과자의 길이가 하나라도 다르면, 싸움이 난다
-	 * 	그래서 모든 조카들에게 같은 길이의 과자를 나눠줘야 한다
-	 *
-	 *
-	 */
+	static int N;
+	static int M;
+	static int[] cookies;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int M = Integer.parseInt(st.nextToken()); // 조카 수
-		int N = Integer.parseInt(st.nextToken()); // 과자의 수
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 
-		int[] cookies = new int[N];
+		cookies = new int[N];
 
 		st = new StringTokenizer(br.readLine());
 
-		int maxLen = 0;
+		int MAX = Integer.MIN_VALUE;
 
-		for (int i = 0; i < N; i++) {
+		for(int i = 0; i < N; i++) {
 			cookies[i] = Integer.parseInt(st.nextToken());
-
-			if (maxLen < cookies[i]) {
-				maxLen = cookies[i];
+			if(cookies[i] > MAX) {
+				MAX = cookies[i];
 			}
 		}
 
-		//  이분탐색으로 mid 값으로 구하려는 것 : 줄 수 있는 최대한 길이
-		int left = 1;
-		int right = maxLen;
+		// 나눠 줄 수 있는 과자의 길이는 양수이기 때문에
+		long left = 1;
+		long right = MAX;
 
-		int ans = 0;
+		long answer = 0;
 
 		while(left <= right) {
+			long mid = (left + right) / 2;
 
-			long cnt = 0;
+			long count = 0 ;
 
-			int mid = (left + right) / 2;
-
-			for(int x : cookies) {
-				cnt = cnt + (x / mid);
+			for(int cookie : cookies) {
+				count = count + (cookie / mid);
 			}
 
-			if(cnt >= M) {
-				ans = mid;
+			// 조카들에게 충분히 과자를 나눠줄 수 있다면 더 큰값으로 시도해본다
+			if(count >= M) {
+				answer = mid;
 				left = mid + 1;
 			} else {
 				right = mid - 1;
 			}
+
 		}
 
-		System.out.println(ans);
+		System.out.println(answer);
+
 	}
 }

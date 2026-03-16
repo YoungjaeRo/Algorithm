53f6e4396096fc110a79e3dd5cbfaec70795738b
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-16 20:39:08 +0900

    [Silver I] Title: 스타트링크, Time: 200 ms, Memory: 58112 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -2,67 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int F;
-	static int S;
-	static int G;
-	static int U;
-	static int D;
 
-	static int[] dist;
-
-	static int[] dx = {-1, 1};
+	static int F; // 건물 총 층수
+	static int S; // 강호의 시작 위치
+	static int G; // 스타트링크의 위치
+	static int U; // 한번에 올라가는 칸수
+	static int D; // 한번에 내려가는 칸수
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		F = Integer.parseInt(st.nextToken()); // 10
-		S = Integer.parseInt(st.nextToken()); // 1
-		G = Integer.parseInt(st.nextToken()); // 10
-		U = Integer.parseInt(st.nextToken()); // 2
-		D = Integer.parseInt(st.nextToken()); // 1
+		F = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		G = Integer.parseInt(st.nextToken());
+		U = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
 
-		if(S == G) { // 이미 같은 층에 있었다며느 0을 출력하고 메서드 종료
-			System.out.println(0);
-			return;
-		}
 
-		dist = new int[F + 1]; // 해당 층까지 도달하는데, 누른 버튼의 수
+		int[] click = new int[F + 1];
+		Arrays.fill(click, -1);
 
-		Queue<Integer> q = new LinkedList<>();
+		click[S] = 0;
+
+		// BFS 시작
+		Queue<Integer> q = new ArrayDeque<>();
 		q.offer(S);
-		dist[S] = 0; //  이미 있는 층은 0임
 
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 
-			// 올라갈때는 cur + U
-			int up = cur + U;
-
-			if(U > 0 && up <= F && dist[up] == 0) {
-				dist[up] = dist[cur] + 1;
-
-				if(up == G) {
-					System.out.println(dist[up]);
-					return;
-				}
-				q.offer(up);
+			if(cur == G) {
+				System.out.println(click[cur]);
+				return;
 			}
 
-			int down = cur - D;
-			if(D > 0 && down >= 1 && dist[down] == 0) {
-				dist[down] = dist[cur] + 1;
+			int[] options = {cur + U, cur - D};
 
-				if(down == G) {
-					System.out.println(dist[down]);
-					return;
+			for(int next : options) {
+				// 범위 검증부터
+				if(next < 1 || next > F) {
+					continue;
 				}
 
-				q.offer(down);
+				// 이미 방문한적이 있어도 스킵
+				if(click[next] != -1) {
+					continue;
+				}
+
+				click[next] = click[cur] + 1;
+				q.offer(next);
 			}
 		}
 
-		// 이 마저도 도달하지 못했으면 계단을 이용
 		System.out.println("use the stairs");
 	}
 }

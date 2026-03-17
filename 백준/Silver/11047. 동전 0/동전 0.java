75629e64acdf5727411e47155584047c303f0aeb
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-17 18:18:20 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,30 +1,45 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	/**
+	 * 동전의 개수를 최저로 쓰기 위해선, 단위가 큰 동전부터 우선해서 사용하면 된다
+	 * (그리디) : 매 라운드 마다 최선의 선택을 하는게 더 좋다
+	 */
 
-        int N = sc.nextInt();
+	static int N; // 동전 가지수
+	static int K; // 만들어야 하는 금액
 
-        int K = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] A = new int[N];
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < N; i++){
-            A[i] = sc.nextInt();
+		List<Integer> coins = new ArrayList<>();
 
-        }
+		for(int i = 0; i < N; i++) {
+			coins.add(Integer.parseInt(br.readLine()));
+		}
 
-        //그리디 알고리즘 --> 최대한 큰 동전 먼저 사용하기(뒤에서부터 탐색하기)
-        int count = 0;
-        for(int i = N - 1; i >= 0; i--){
-            if(A[i] <= K) {
-                count = count + (K / A[i]);
-                K = K % A[i];
-            }
-        }
-        System.out.println(count);
+		// 내림 차순으로 재정렬 해주기
+		Collections.sort(coins, Collections.reverseOrder());
 
-    }
+		// 환전되는 동전의 수
+		int answer = 0;
+
+		// 잔액
+		int remaining = K;
+
+		for(int coin : coins) {
+
+			if(coin <= remaining) {
+				answer = answer + (remaining / coin);
+				remaining = remaining % coin;
+			}
+		}
+
+		System.out.println(answer);
+	}
 }

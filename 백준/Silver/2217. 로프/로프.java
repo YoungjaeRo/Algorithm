49a523611da52b0bb5845ac11ab3bc9c51573f8a
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-17 18:33:35 +0900

    [Silver IV] Title: 로프, Time: 288 ms, Memory: 26580 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -2,44 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	/**
-	 * 각기 다르게 버틸 수 있는 무게가 다른 로프들이 있다.
-	 * 이 로프들을 활용하여 들어올릴 수 있는 물체의 최대 중량을 구하시오
-	 *
-	 * 로프들이 들어올릴 수 있는 최대 중량
-	 * 20, 10 이면 최대 버틸 수 있는 무게는 10 * 20  = 20
-	 */
 	static int N;
-
-	static Integer[] weights;
-
+	static int[] ropes;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		N = Integer.parseInt(br.readLine());
 
-		weights = new Integer[N];
+		ropes = new int[N];
 
 		for(int i = 0; i < N; i++) {
-			int weight = Integer.parseInt(br.readLine());
-			weights[i] = weight;
+			ropes[i] = Integer.parseInt(br.readLine());
 		}
 
-		// 내림차순 : 강한 로프부터 하나씩 추가해보는 게 직관적
-		Arrays.sort(weights, Collections.reverseOrder());
+		// 오름차순으로 정렬하기
+		Arrays.sort(ropes);
 
-		long answer = 0;
+		// 최대값
+		int max = Integer.MIN_VALUE;
 
 		for(int i = 0; i < N; i++) {
-			long weakest = weights[i]; // 현재 선택한 로프중 가장 최약
-			long pick = i + 1; // 선택한 로프 개수
-			long canLift = weakest * pick;
 
-			answer = Math.max(answer, canLift);
+			int possibleWeigth = ropes[i] * (N - i);
+
+			max = Math.max(max, possibleWeigth);
 		}
 
-		System.out.println(answer);
+		System.out.println(max);
 	}
 }

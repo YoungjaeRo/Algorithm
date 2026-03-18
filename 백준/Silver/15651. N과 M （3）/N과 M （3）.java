c3895328adde07c0c988f293f7f634896a2b613e
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-18 17:44:29 +0900

    [Silver III] Title: N과 M (3), Time: 396 ms, Memory: 68436 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,45 +1,51 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
+	/**
+	 * 같은 수를 여러번 골라도 된다
+	 *
+	 * 중복은 허용하지 않음
+	 *
+	 * 순서가 중요함 --> 순열 문제임
+	 */
 	static int N;
 	static int M;
 
-	static int[] arr; // 뽑은 숫자들을 저장해놓을 배열
+	static int[] pick;
+
 	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		arr = new int[M];
+		pick = new int[M];
 
-		dfs(0);
+
+		backtrack(0);
 
 		System.out.println(sb);
-
 	}
 
-	static void dfs(int depth) { // 0-- >
+	static void backtrack(int depth) {
 
-		//1. 종료 조건 부터
+		// 길이 M만큼 다 골랐을 때
 		if(depth == M) {
-			for(int i : arr) {
-				sb.append(i).append(" ");
+			for(int p : pick) {
+				sb.append(p).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
 
-		for(int i = 1; i <= N; i++) {// 0
-			// 같은 숫자를 쓰는 경우
-			arr[depth] = i;
-			dfs(depth + 1); // dfs(0) --
+		for(int i = 1; i <=N; i++) {
+			pick[depth] = i;
+			backtrack(depth + 1);
 		}
 	}
 }

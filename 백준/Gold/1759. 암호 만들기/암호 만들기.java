f85f4a53d6599f350543c27cf8e03bb703cca1cb
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-18 17:02:16 +0900

    [Gold V] Title: 암호 만들기, Time: 104 ms, Memory: 14836 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -3,45 +3,40 @@ import java.util.*;
 
 public class Main {
 	/**
-	 * 암호를 만드는데,
-	 * 서로 다른 L개의 알파벳 소문자들로 구성
+	 * 최소 한개의 모음(a, e, i, o, u)와 2개의 자음으로 구성되어 있어야 하므로
 	 *
-	 * a,e,i,o,u에서 최소 한개
-	 *
-	 * 최소 두개는 나머지 자음에서
-	 *
-	 * 알파벳이 오름차순으로 ㅇㅇ
-	 *
-	 * 일단은 조합문제임
-	 *
-	 * C개 중 L개를 선택해서 문제를 구하시오 ㅇㅇ
+	 * 나중에 depth에 도달하면, 구성 요소들의 모음과 자음 현황을 검사한다. ㅇㅇ
 	 */
 
-	static int C;
-	static int L;
+	static int L; // 암호문의 길이(depth)
+	static int C; // 주어진 문자들 개수
 
-	static char[] chars;
-	static char[] pick;
+	static String[] chars;
+	static String[] pick;
 
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		L = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
-		chars = new char[C];
-		pick = new char[L];
+		// 후보 문자들을 저장해놓을 배열 초기화
+		chars = new String[C];
+
+		// 선택 현황을 담아놓을 배열 초기화
+		pick = new String[L];
+
 
 		st = new StringTokenizer(br.readLine());
 
 		for(int i = 0; i < C; i++) {
-			chars[i] = st.nextToken().charAt(0); // 이렇게 해야, String에서 char로 변형이 된다
+		chars[i] = st.nextToken();
 		}
 
+		// 오름차순으로 정렬해주기
 		Arrays.sort(chars);
 
 		backtrack(0, 0);
@@ -49,35 +44,40 @@ public class Main {
 		System.out.println(sb);
 	}
 
-
-	static boolean isVowel(char c) {
-		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-	}
-
 	static void backtrack(int depth, int start) {
 		if(depth == L) {
-			int vowel = 0;
-			int consonant = 0;
-
-			for(char p : pick) {
-				if(isVowel(p)) {
-					vowel++;
-				} else {
-					consonant++;
+			if(isValid(pick)){
+				for(String s : pick) {
+					sb.append(s);
 				}
-			}
-
-			if(vowel >= 1 && consonant >= 2) {
-				sb.append(pick).append("\n");
+				sb.append("\n");
 			}
 			return;
 		}
 
-
-		for(int i = start; i < chars.length; i++) {
+		for(int i = start;  i < chars.length; i++) {
 			pick[depth] = chars[i];
 
 			backtrack(depth + 1, i + 1);
+		}
+	}
+
+	static boolean isValid(String[] pick) {
+		int moCount = 0;
+		int jaCount = 0;
+
+		for(String s : pick) {
+			if(s.equals("a") || s.equals("e") || s.equals("i") || s.equals("o") || s.equals("u")) {
+				moCount++;
+			} else {
+				jaCount++;
+			}
+		}
+
+		if(moCount >= 1 && jaCount >= 2) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }

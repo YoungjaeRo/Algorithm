06b312705498ee7401b79305ec2cc398ab9039fe
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-19 20:27:39 +0900

    [Silver II] Title: 결혼식, Time: 168 ms, Memory: 18448 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -3,75 +3,82 @@ import java.util.*;
 
 public class Main {
 	/**
-	 * 상근이 부터 시작해서, 친구의 친구 즉, 인접리스트를 통해서 풀면된다 ㅇㅇ
+	 * 친구와 친구의 친구까지만 초대하기로 함
+	 *
+	 * 시작점이 정해져 있는 BFS를 하면 됨, 가중치가 1로 일정하기 때문에
+	 * 다익스트라는 굳이이다.
 	 */
 
-	static int N;
+	static int N; // 상근이 포함 동기 수 (상근이는 1번)
 
-	static int M;
+	static int M; // 리스트의 길이
 
-	static List<Integer> [] list;
+	// 상근이와 떨어져 있는 정도 + 방문여부 확인용 배열
+	static int[] dist;
+
+	// 인접리스트
+	static List<Integer>[] graph;
+
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 
-		list = new ArrayList[N + 1];
+		graph = new ArrayList[N + 1];
 
-		// 인접리스트 초기화
+		// 인접리스트 선언해주기
 		for(int i = 1; i <= N; i++) {
-			list[i] = new ArrayList<>();
+			graph[i] = new ArrayList<>();
 		}
 
-		// 친구 관계 입력
+		StringTokenizer st;
+
+		// 서로 친구 관계 주입해주기
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
-			list[b].add(a);
-		}
-		// 1번부터 i까지 동기들에게 닿는데, 걸리는 거리 여기서 1 또는 2까지로 제한해야한다.
 
-		int[] dist = new int[N + 1];
+			graph[a].add(b);
+			graph[b].add(a);
+		}
+
+		dist = new int[N + 1];
+
 		Arrays.fill(dist, -1);
 
-		// BFS 시작
-		Queue<Integer> q = new ArrayDeque<>();
+		bfs(1);
 
-		int start = 1; // 1번째 동기부터 탐색 시작
-
-		dist[start] = 0; // 시작점 거리 0
-
-		q.add(start);
-
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-
-			if(dist[cur] == 2) {  // cur이 이미 2촌이면 여기서 더 퍼지면 3촌이 생김 -> stop
-				continue;
-			}
-
-			for(int next : list[cur]) {
-				if(dist[next] == -1) { // 한번도 방문한적이 없다면,
-					dist[next] = dist[cur] + 1;
-					q.add(next);
-				}
-			}
-		}
-
+		// 결혼식에 초대할 동기수
 		int answer = 0;
-		for(int i = 2; i <= N; i++) {
+
+		for(int i = 2; i<= N; i++) {
 			if(dist[i] == 1 || dist[i] == 2) {
 				answer++;
 			}
 		}
 
 		System.out.println(answer);
+	}
 
+	static void bfs(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		dist[start] = 0;
+		q.offer(start);
+
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+
+			for(int next : graph[cur]) {
+				// 아직 방문하지 않았다면,
+				if(dist[next] == -1) {
+					dist[next] = dist[cur] + 1;
+
+					q.offer(next);
+				}
+			}
+		}
 	}
 }

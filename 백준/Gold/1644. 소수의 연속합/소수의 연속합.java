63cc0521e1e2cb7b5642074f63a8bfbd3e3270b3
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-25 18:20:18 +0900

    [Gold III] Title: 소수의 연속합, Time: 256 ms, Memory: 29252 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 	/**
-	 *  해당 자연수 N을 연속된 소수로 나타내는 법
+	 * N 이하의 소수를 구하는 공식인 에라토스테네스의 채를 활용해 풀면 훨씬 쉽다.
 	 */
 
 	static int N;
@@ -12,71 +12,58 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 
-		if(N < 2) {
-			System.out.println(0);
-			return;
-		}
 
-		/**
-		 * 해당 ~ N까지의 범위중, 소수들만 탐색하기 위해, 에라토스테네스의 채를 활용한다
-		 */
-
-		// 에라토스테네스의 채 : N 이하의 소수 구하기
 		boolean[] isPrime = new boolean[N + 1];
 		Arrays.fill(isPrime, true);
 
 		isPrime[0] = false;
 		isPrime[1] = false;
 
-		/**
-		 * 에라토스테네스의 채
-		 */
-
-		// 가장 중요한 부분인데, i 는 sqrt(N)까지만 보면 된다
+		// 에라토스테네스의 채
 		for(int i = 2; i * i <= N; i++) {
-			if(!isPrime[i]) {
-				continue;
-			}
 
-			// i * i 부터 시작해서 그 외  + i를 통해 i 배수들을 지워나간다.
 			for(int j = i * i; j <= N; j = j + i) {
 				isPrime[j] = false;
 			}
 		}
 
-		// 소수들 리스트에 다 삽입
-		ArrayList<Integer> primes = new ArrayList<>();
+		List<Integer> primes = new ArrayList<>();
+
 		for(int i = 2; i <= N; i++) {
 			if(isPrime[i]) {
 				primes.add(i);
 			}
 		}
 
-		// 2. 투 포인터를 활용해서, 연속된 소수 합이 N 인 경우를 카운트
+		// 투포인터 시작
 		int left = 0;
 		int right = 0;
 		int sum = 0;
 		int count = 0;
 
 		while(true) {
+
 			if(sum >= N) {
-				if(sum == N) { // 갑이 일치하, 카운트
+				if(sum == N) {
 					count++;
 				}
-
-				// sum이 너무 크거나 같으면, 왼쪽을 줄여서 합을 낮춘다
-				sum = sum - primes.get(left); // 왼쪽 줄이기
+				
+				sum = sum - primes.get(left);
 				left++;
-
 			} else {
+				
 				if(right == primes.size()) {
 					break;
 				}
+				
 				sum = sum + primes.get(right);
 				right++;
 			}
+
 		}
 
 		System.out.println(count);
+
+
 	}
 }

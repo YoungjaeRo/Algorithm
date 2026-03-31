cb1571e16dbffac2571528050664e6adf49b71fb
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2026-03-31 14:38:03 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 536 ms, Memory: 57244 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -2,45 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	/**
-	 * 동물원에서 막 탈출한 원숭이 한마리
+	 * 말의 움직임으로 K 번까지 이동이 가능하고, 그 외의 이동은 인접한 곳으로만 이동이 가능함
 	 *
-	 * 말은 체스에서 나이트와 같은 이동을 한다
+	 * 2,2 기준점 기준
+	 * 말이 이동 가능한 칸은
 	 *
-	 * 원숭이는 딱 K번까지만 말처럼 이동이 가능한다
-	 * 그 외는 인접한 칸(동, 서, 남, 북)으로 이동이 가능함
-	 * 원숭이는 여행을 떠나는데, 격자판의 맨 왼쪽위에서 시작해서
-	 * 맨 오른쪽 아래까지 가야한디
+	 * 1 , 0
+	 * 0, 1
 	 *
-	 * 맵 상으로는 0, 0 -- > (H - 1, W - 1)까지 이동함
+	 * 0, 3
+	 * 1, 4
 	 *
-	 * 최소한의 동작 -- > BFS
+	 * 3, 0
+	 * 4, 1
 	 *
-	 * 0은 평지, 1은 장애물을 뜻한다
+	 * 3, 4
+	 * 4, 3
 	 */
 
-	static class Node {
-		int r;
-		int c;
-		int kUsed;
-		int time;
+	static int[] hx = {-1, -2, -2, -1, 1, 2, 1, 2};
+	static int[] hy = {-2, -1, 1, 2, -2, -1, 2, 1};
 
-		Node(int r, int c, int kUsed, int dist) {
-			this.r = r;
-			this.c = c;
-			this.kUsed = kUsed;
-			this.time = dist;
-		}
-	}
-
-	// 말의 이동
-	static int[] xh = {-2, -2, -1, -1, 1, 1, 2, 2};
-	static int[] yh = {-1, 1, -2, 2, -2, 2, -1, 1};
-
-	// 원숭이(상하좌우) 이동
-	static final int[] dr4 = {-1, 1, 0, 0};
-	static final int[] dc4 = {0, 0, -1, 1};
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
 
 	static int K;
 	static int W;
@@ -48,11 +33,25 @@ public class Main {
 
 	static int[][] map;
 
-	// kUsed, x, y
-	static boolean[][][] visited;
+	static boolean[][][] visited; // 행과 열 그리고 말로써의 이동 횟수까지
+	static int[][][] dist;
+
+
+	static class Node {
+		int x;
+		int y;
+		int kTime;
+
+		Node(int x, int y, int kTime) {
+			this.x = x;
+			this.y = y;
+			this.kTime = kTime;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		K = Integer.parseInt(br.readLine());
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -62,7 +61,12 @@ public class Main {
 
 		map = new int[H][W];
 
-		// 그래프 정보 주입 (0은 평지, 1은 장애물)
+		// 0회도 따로 기록해야하기 때문에, K + 1칸으로 스케일링
+		visited = new boolean[H][W][K + 1];
+		dist = new int[H][W][K + 1];
+
+
+		// 맵정보 입력 받기 0은 평지, 1은 장애물
 		for(int i = 0; i < H; i++) {
 			st = new StringTokenizer(br.readLine());
 
@@ -70,85 +74,87 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+
 		int answer = bfs();
 		System.out.println(answer);
 	}
 
 	static int bfs() {
 		Queue<Node> q = new ArrayDeque<>();
-		visited = new boolean[K + 1][H][W];
 
 		visited[0][0][0] = true;
-		q.offer(new Node(0, 0, 0, 0));
+		q.offer(new Node(0, 0, 0));
 
 		while(!q.isEmpty()) {
-			Node cur = q.poll();
+			Node n = q.poll();
 
-			if(cur.r == H - 1 && cur.c == W - 1) {
-				return cur.time;
+			int cx = n.x;
+			int cy = n.y;
+			int curK = n.kTime;
+
+			// 해당 지점에 도착했으면, 바로 리턴해주기
+			if(cx == H - 1 && cy == W - 1) {
+				return dist[cx][cy][curK];
 			}
 
-			// 원숭이처럼 인접 이동
+			// 원숭이 루트로 탐색 시작
 			for(int d = 0; d < 4; d++) {
-				int nr = cur.r + dr4[d];
-				int nc = cur.c + dc4[d];
+				int nx = cx + dx[d];
+				int ny = cy + dy[d];
 
-				//  범위 검증하가
-				if(!inRange(nr, nc)) {
+				// 범위 검증부터
+				if(nx < 0 || ny < 0 || nx >= H || ny >= W) {
 					continue;
 				}
 
-				// 이미 방문한 것인지 검증
-				if(visited[cur.kUsed][nr][nc]) {
+				// 이미 방문한적이 있다면
+				if(visited[nx][ny][curK]) {
 					continue;
 				}
 
-				// 벽이면 이동 못함
-				if(map[nr][nc] == 1) {
+				// 벽으로 막혀있어도 이동 불가
+				if(map[nx][ny] == 1) {
 					continue;
 				}
 
-				visited[cur.kUsed][nr][nc] = true;
-				q.offer(new Node(nr, nc, cur.kUsed, cur.time + 1));
+				visited[nx][ny][curK] = true;
+				dist[nx][ny][curK] = dist[cx][cy][curK] + 1;
+				q.offer(new Node(nx, ny, curK));
 			}
 
 
-			// 말처럼 이동
-			if(cur.kUsed < K) {
-
-				int nextK = cur.kUsed+ 1;
+			// 말로 이동하는 경로도 넣어주기
+			if(curK < K) {
 
 				for(int d = 0; d < 8; d++) {
-					int nr = cur.r + xh[d];
-					int nc = cur.c + yh[d];
+					int nx = cx + hx[d];
+					int ny = cy + hy[d];
+					int nk = curK + 1;
 
-					// 범위확인부터
-					if(!inRange(nr, nc)) {
+					// 범위검증부터
+					if(nx < 0 || ny < 0 || nx >= H || ny >= W) {
 						continue;
 					}
 
-					if(visited[nextK][nr][nc]) {
+					// 이미방문한적이 있다면 스킵
+					if(visited[nx][ny][nk]) {
 						continue;
 					}
 
-
-					if(map[nr][nc] == 1) {
+					// 최종 도착지가 벽이라면 이동불가
+					if(map[nx][ny] == 1) {
 						continue;
 					}
 
-					visited[nextK][nr][nc] = true;
-					q.offer(new Node(nr, nc, nextK, cur.time + 1));
+					visited[nx][ny][nk] = true;
+					dist[nx][ny][nk] = dist[cx][cy][curK] + 1;
+					q.offer(new Node(nx, ny, nk));
 				}
 			}
+
 		}
 
-		// BFS가 끝났는데도, 변화가 없다면 -1을 출력
 		return -1;
 
-
-	}
-
-	static boolean inRange(int x, int y) {
-		return x >= 0 && y >= 0 && x < H && y < W;
 	}
 }
